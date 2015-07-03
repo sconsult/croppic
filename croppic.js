sconsult/croppic

commit d400fd8273566815c29b18b65162b23f67e781aa

Fixed Drag Picture support for touch screen laptops without IOS or Android User Agent

croppic.js
@@ -444,7 +444,7 @@
 				var pageX;
                 var pageY;
                 var userAgent = window.navigator.userAgent;
-                if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || userAgent.match(/android/i)) {
+                if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || userAgent.match(/android/i) || (e.pageY && e.pageX) == undefined) {
                     pageX = e.originalEvent.touches[0].pageX;
                     pageY = e.originalEvent.touches[0].pageY;
                 } else {
@@ -463,7 +463,7 @@
 					var imgTop;
 					var imgLeft;
 					
-					if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || userAgent.match(/android/i)) {
+					if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || userAgent.match(/android/i) (e.pageY && e.pageX) == undefined) {
                         imgTop = e.originalEvent.touches[0].pageY + pos_y - drg_h;
                         imgLeft = e.originalEvent.touches[0].pageX + pos_x - drg_w;
                     } else {

commit 906a0121d1ef90aabf8c0f081f5cd9211dc98cc4

Fixed bug introduced in d400fd8273566815c29b18b65162b23f67e781aa

croppic.js
@@ -464,7 +464,7 @@
 					var imgTop;
 					var imgLeft;
 					
-					if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || userAgent.match(/android/i) (e.pageY && e.pageX) == undefined) {
+					if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || userAgent.match(/android/i) || (e.pageY && e.pageX) == undefined) {
                         imgTop = e.originalEvent.touches[0].pageY + pos_y - drg_h;
                         imgLeft = e.originalEvent.touches[0].pageX + pos_x - drg_w;
                     } else {